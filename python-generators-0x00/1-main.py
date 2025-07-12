df570a7f03879778ee0a3375d748157c9134b754
Joseph Ahor Abandoh-Sam <abandoh@gmail.com>
2025-07-12 19:56:12 +0000

    corrected 1-main.py to display first 6 and related files

--- python-generators-0x00/1-main.py
+++ python-generators-0x00/1-main.py
@@ -1,6 +1,8 @@
 
 from itertools import islice
-stream_users = __import__('0-stream_users')
+from streamer_users import stream_users
+# stream_users = __import__('0-stream_users')
+
 
 # iterate over the generator function and print only the first 6 rows
 
